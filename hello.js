4d6a33bc2caac9e7668fc6797a68fabf960df7c9
Edree Huss <edreeshuss@gmail.com>
2020-10-22 10:12:28 -0400

    [Hello] Add hello functions

---
+++ hello.js
@@ -0,0 +1,33 @@
+function sayHello(name) {
+    let msg = 'hello ' + name+ '. How are you?';
+    return msg;
+}
+console.log(sayHello('SEI 1019'));
+
+function sayHello(name) {
+    let msg = 'hello ' + name+ '. How are you?';
+    return msg;
+}
+console.log(sayHello('Classmates'));
+
+
+function sayHello(name) {
+    let msg = 'hello ' + name+ '. How are you?';
+    return msg;
+}
+console.log(sayHello('team'));
+
+
+{function sayHello(name) {
+    let msg = 'hello ' + name+ '. How are you?';
+    return msg;
+}
+}
+console.log(sayHello('crew'));
+
+{function sayHello(name) {
+    let msg = 'hello ' + name+ '. How are you?';
+    return msg;
+}
+}
+console.log(sayHello('squad'));
